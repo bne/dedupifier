807002fd312e22da04fd6779ea6c2f361bb0d5b9
Ben Miller <ben@hyl.co.uk>
2016-03-25 01:24:57 +0000

    hash dedupe

--- dedupifier/tests/test_dedupe.py
+++ dedupifier/tests/test_dedupe.py
@@ -8,11 +8,19 @@ class DedupeTestCase(unittest.TestCase):
 	def setUp(self):
 		path = os.path.join(os.path.dirname(__file__), 'fixtures')		
 		self.dedupifier = dedupe.Dedupifier(path)
-		self.dedupifier.dedupify()		
 
-	def test_find_duplicates(self):
+	def test_find_duplicates_by_name(self):
+		self.dedupifier.get_items()
+
 		self.assertEqual(len(self.dedupifier.items), 3)
+		self.assertEqual(len(self.dedupifier.items['Bennyisawesome.jpg']), 6)
+		self.assertEqual(len(self.dedupifier.items['blackmagic_hotpixel_test.jpg']), 2)
+		self.assertEqual(len(self.dedupifier.items['hunger-games.png']), 5)
 
-
-
-
+	def test_find_duplicates_by_hash(self):
+		self.dedupifier.get_items(use_hash=True)
+				
+		self.assertEqual(len(self.dedupifier.items), 3)
+		self.assertEqual(len(self.dedupifier.items['07486b2d43fe6e8bbba0cf500c56db01']), 6)
+		self.assertEqual(len(self.dedupifier.items['9ec4f5e7da187ff44d179d85aac654e4']), 2)
+		self.assertEqual(len(self.dedupifier.items['c714aa4f97b706b2cd07b360f2fd1dbb']), 5)
