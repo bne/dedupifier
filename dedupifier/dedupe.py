b7cb93b93e4951efe4a226f66fa4ef1dae361aac
Ben Miller <ben@hyl.co.uk>
2016-04-11 12:43:39 +0100

    log

--- dedupifier/dedupe.py
+++ dedupifier/dedupe.py
@@ -3,23 +3,31 @@ import sys
 import hashlib
 import argparse
 import itertools
-
+import logging
 
 spinner = itertools.cycle(['-', '/', '|', '\\'])
-
+logger = logging.getLogger()
 
 class Dedupifier(object):
 
     block_size = 65536
 
-    def __init__(self, path, verbosity=0, use_hash=False):
+    def __init__(self, path, verbosity=0, use_hash=False, log_file=None):
         self.path = path
         self.verbosity = verbosity
         self.use_hash = use_hash
+        self.log_file = log_file
 
     def spin(self):
         sys.stdout.write('{}\b'.format(spinner.__next__()))
         sys.stdout.flush()
+
+    def write_to_log(self):
+        with self.log_file as f:
+            for item in self.items:
+                f.write('{}\n'.format(item))
+                for file in self.items[item]:
+                    f.write('  {}\n'.format(file))
 
     def hash_file(self, path):
         _hash = hashlib.md5()
@@ -46,6 +54,7 @@ class Dedupifier(object):
             for filename in files:
 
                 num_files += 1
+                key = None
 
                 if self.verbosity < 2:
                     self.spin()
@@ -53,22 +62,31 @@ class Dedupifier(object):
                 full_path = os.path.join(root, filename)
 
                 if self.use_hash:
-                    key = self.hash_file(full_path)
+                    try:
+                        key = self.hash_file(full_path)
+                    except (FileNotFoundError, OSError) as exc:
+                        logger.error("{}: '{}'".format(exc.strerror, exc.filename))
+
                 else:
                     key = filename
 
-                if not key in self.items:
-                    num_keys += 1
-                    if self.verbosity > 1:
-                        sys.stdout.write('Adding new item {} from {}\n'.format(key, full_path))
-                    self.items[key] = []
+                if key:
+                    if not key in self.items:
+                        num_keys += 1
+                        if self.verbosity > 1:
+                            sys.stdout.write('Adding new item {} from {}\n'.format(key, full_path))
+                        self.items[key] = []
 
-                self.items[key].append(full_path)
+                    self.items[key].append(full_path)
 
         if self.verbosity < 2:
             sys.stdout.write('Found {} items in {} files\n'.format(num_keys, num_files))
 
-        sys.stdout.write('\nDone\n')
+        if self.log_file:
+            sys.stdout.write('Writing results to {}\n'.format(self.log_file.name))
+            self.write_to_log()
+
+        sys.stdout.write('Done\n')
 
         return self.items
 
@@ -77,22 +95,15 @@ def main():
     parser = argparse.ArgumentParser(description='Dedupifier')
     parser.add_argument('path', help='directory to dedupe')
     # warning: twisted logic
-    parser.add_argument('-x', dest='hash', action='store_false', default=True,
+    parser.add_argument('-x', dest='use_hash', action='store_false', default=True,
         help='use filename as a key instead of hash')
-    # parser.add_argument('-l', dest='logfile', default='dedupe.log',
-    #     type=argparse.FileType('w'), help='save to logfile')
+    parser.add_argument('-l', dest='log_file', default='dedupe.log',
+        type=argparse.FileType('w'), help='save to logfile')
     parser.add_argument('-v', dest='verbosity', default=0, action='count', help='verbosity')
     args = parser.parse_args()
 
-    dedupifier = Dedupifier(args.path, verbosity=args.verbosity, use_hash=args.hash)
+    dedupifier = Dedupifier(args.path, verbosity=args.verbosity, use_hash=args.use_hash, log_file=args.log_file)
     dedupifier.get_items()
-
-    # for item in dedupifier.items:
-    #     args.logfile.write('{}\n'.format(item))
-    #     for file in dedupifier.items[item]:
-    #         args.logfile.write('    {}\n'.format(file))
-
-    # args.logfile.close()
 
 
 if __name__ == "__main__":
